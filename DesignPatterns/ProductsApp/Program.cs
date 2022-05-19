9147ec5f14b1b5247eed2e99ab2db1e41091f1cb
Magesh Kuppan <tkmagesh77@gmail.com>
2022-05-19 15:10:21 +0530

    products sort implementation

---
+++ DesignPatterns/ProductsApp/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Cost { get; set; }
+    public string Category { get; set; }
+
+    public override string ToString()
+    {
+        return $"Id={Id}, Name={Name}, Cost={Cost}, Category={Category}";
+    }
+}
+
+public class Products
+{
+    private IList<Product> list = new List<Product>();
+
+    public IList<Product> GetAll
+    {
+        get => list;
+    }
+
+    public void Sort()
+    {
+
+    }
+
+}
+
