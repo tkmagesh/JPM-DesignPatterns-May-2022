c2cf663ec2922ed2a02c97e14c499b5b04ad4f15
Magesh Kuppan <tkmagesh77@gmail.com>
2022-05-20 13:07:17 +0530

    Decorator

---
+++ DesignPatterns/02-Structural-Patterns/Decorator/Program.cs
@@ -0,0 +1,45 @@
+﻿using Decorator;
+
+Console.Title = "Decorator";
+
+// instantiate mail services
+var cloudMailService = new CloudMailService();
+cloudMailService.SendMail("Hi there.");
+
+var onPremiseMailService = new OnPremiseMailService();
+onPremiseMailService.SendMail("Hi there.");
+
+// add behavior
+var statisticsDecorator = new StatisticsDecorator(cloudMailService);
+statisticsDecorator.SendMail($"Hi there via {nameof(StatisticsDecorator)} wrapper.");
+
+// add behavior
+var messageDatabaseDecorator = new MessageDatabaseDecorator(onPremiseMailService);
+messageDatabaseDecorator.SendMail(
+    $"Hi there via {nameof(MessageDatabaseDecorator)} wrapper, message 1.");
+messageDatabaseDecorator.SendMail(
+    $"Hi there via {nameof(MessageDatabaseDecorator)} wrapper, message 2.");
+
+foreach (var message in messageDatabaseDecorator.SentMessages)
+{
+    Console.WriteLine($"Stored message: \"{message}\"");
+}
+
+
+//Functional Decorators
+Action<int, int> add = (x, y) => Console.WriteLine($"Add result = {x + y}");
+Action<int, int> subtract = (x, y) => Console.WriteLine($"Subtract result = {x + y}");
+
+Func<Action<int, int>, Action<int, int>> decorator = fn => (int x, int y) =>
+{
+    Console.WriteLine("Invocation started");
+    fn(x, y);
+    Console.WriteLine("Invocation completed");
+};
+
+var logAdd = decorator(add);
+var logSub = decorator(subtract);
+
+logAdd(100, 200);
+logSub(100, 200);
+Console.ReadKey();
